f509a0b70c2b6f73f52978dffc82d0f669968221
Administrator <Administrator@PUNEET>
2021-03-09 22:50:58 +0530

    create api class token generation

---
+++ Database/Classes/APIs/APIcalling.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace Database.Classes.APIs
+{
+    class APIcalling
+    {
+        public static string GetResponse(string requestData, string url)
+        {
+            string responseXML = string.Empty;
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(requestData);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.Headers.Add("Accept-Encoding", "gzip");
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(data, 0, data.Length);
+                dataStream.Close();
+                WebResponse webResponse = request.GetResponse();
+                var rsp = webResponse.GetResponseStream();
+                if (rsp == null)
+                {
+                    //throw exception
+                }
+                using (StreamReader readStream = new StreamReader(new GZipStream(rsp, CompressionMode.Decompress)))
+                {
+                    responseXML = JsonConvert.DeserializeXmlNode(readStream.ReadToEnd()).InnerXml;
+                }
+                return responseXML;
+            }
+            catch (WebException webEx)
+            {
+                //get the response stream
+                WebResponse response = webEx.Response;
+                Stream stream = response.GetResponseStream();
+                String responseMessage = new StreamReader(stream).ReadToEnd();
+                return responseMessage;
+            }
+        }
+
+
+    }
+}
---
+++ Database/Classes/APIs/TBO_tokengeneration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Database.Classes.APIs
+{
+    class TBO_tokengeneration
+    {
+
+        
+        string hostName = Dns.GetHostName(); // Retrive the Name of HOST  
+        static string myIP = "";
+        static string  api_name = "http://api.tektravels.com/SharedServices/SharedData.svc/rest/Authenticate";
+        static string json_request = "{\"ClientId\": \"ApiIntegrationNew\",\"UserName\": \"Wing\",\"Password\": \"Wing@1234\",\"EndUserIp\": \""+ myIP + "\"}";
+
+        string response = APIcalling.GetResponse(json_request, api_name);
+
+
+    }
+
+
+}
