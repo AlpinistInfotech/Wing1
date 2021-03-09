457c0b777a938e13d55be347117e75bd7d91a26b
Administrator <Administrator@PUNEET>
2021-03-09 23:04:33 +0530

    api

--- Database/Classes/APIs/TBO_tokengeneration.cs
+++ Database/Classes/APIs/TBO_tokengeneration.cs
@@ -7,15 +7,19 @@ namespace Database.Classes.APIs
 {
     class TBO_tokengeneration
     {
+        public static string TBO_TokenGeneration()
+        {
 
-        
-        string hostName = Dns.GetHostName(); // Retrive the Name of HOST  
-        static string myIP = "";
-        static string  api_name = "http://api.tektravels.com/SharedServices/SharedData.svc/rest/Authenticate";
-        static string json_request = "{\"ClientId\": \"ApiIntegrationNew\",\"UserName\": \"Wing\",\"Password\": \"Wing@1234\",\"EndUserIp\": \""+ myIP + "\"}";
+            string hostName = Dns.GetHostName(); // Retrive the Name of HOST  
+            string myIP = "";
+            string api_name = "http://api.tektravels.com/SharedServices/SharedData.svc/rest/Authenticate";
+            string json_request = "{\"ClientId\": \"ApiIntegrationNew\",\"UserName\": \"Wing\",\"Password\": \"Wing@1234\",\"EndUserIp\": \"" + myIP + "\"}";
 
-        string response = APIcalling.GetResponse(json_request, api_name);
+            string response = APIcalling.GetResponse(json_request, api_name);
+            string token_value = "";
 
+            return token_value;
+        }
 
     }
 
